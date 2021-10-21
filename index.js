2e587eb11f9a2481e93b4f5eb8e7b49632e30ada
Nick67644 <24.mathman890.mathman@gmail.com>
2021-10-21 11:05:22 -0500

    Update index.js
    Updated count, Adding Image Text, Updated Image/Small Image.

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-  
+
 const RPC = require('discord-rpc');
 const client = new RPC.Client({ transport: 'ipc' });
 client.on('ready', () =>
 {
-    client.request('SET_ACTIVITY', {
-        pid: process.pid,
-        activity: {
-            assets: {
-                large_image: 'pycord_logo' //woc
-            },
-            details: '8k+ CODING SERVER!', //Pycord new Discord.py!
-            buttons: [
-                { label: 'Join World of Coding!', url: 'https://discord.gg/pkShK9ax2X' }, //woc (Partner of Pycord)
-                { label: 'Join Pycord!', url: 'https://discord.gg/pycord'} //Pycord 
-            ]
-        }
-    });
-    console.log('started!');
+  client.request('SET_ACTIVITY', {
+      pid: process.pid,
+      activity: {
+          assets: {
+              large_image: 'woc', //woc
+              small_image: 'pycord_logo',
+              small_text: 'Pycord!',
+              large_text: "World of Coding!",
+          },
+
+          details: '10k+ CODING HELP SERVER!', //Pycord new Discord.py!
+          buttons: [
+              { label: 'Join World of Coding!', url: 'https://discord.gg/pkShK9ax2X' }, //woc (Partner of Pycord)
+              { label: 'Join Pycord!', url: 'https://discord.gg/dDvdeA8Jqz'} //Pycord
+          ]
+      }
+  });
+  console.log('started!');
 });
 client.login({ clientId: '803978215466074122' });
